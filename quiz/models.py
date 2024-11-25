1335e3d0e6b0720a1664229fe122890ee27a87c7
binler <letatbinh130794@gmail.com>
2024-11-26 00:03:32 +0700

    feat: add timestamp to question

--- quiz/models.py
+++ quiz/models.py
@@ -3,9 +3,17 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.question_text
+
+    def get_created_at_vn(self):
+        return self.created_at.strftime("%d/%m/%Y %H:%M:%S")
+
+    def get_updated_at_vn(self):
+        return self.updated_at.strftime("%d/%m/%Y %H:%M:%S")
 
 
 class Choice(models.Model):
